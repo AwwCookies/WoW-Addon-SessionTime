5a6ea8ad27c303a720e27bdd25aaa5b89cf65c8e
AwwCookies <emma@emaw.dev>
2022-10-22 02:02:07 -0400

    Added Addon Settings

--- SessionTime.lua
+++ SessionTime.lua
@@ -1,8 +1,74 @@
 local f = CreateFrame('Frame', 'SessionTimeFrame')
 local t = f:CreateFontString(nil, "OVERLAY", "GameFontNormalTiny")
+f:RegisterEvent("ADDON_LOADED")
 
-t:SetTextColor(255, 255, 255, 0.7)
-t:SetPoint("BOTTOMLEFT", CharacterMicroButton, "LEFT", -35, -4)
+local defaultSettings = {
+    fontSize = 10,
+    offsetX = -40,
+    offsetY = -4   
+}
+
+SessionTimeVars = SessionTimeVars or defaultSettings
+
+
+
+
+-- AddOn Settings
+local function createAddonSetting()
+    local category = Settings.RegisterVerticalLayoutCategory("Session Time")
+
+    -- Font Size
+    do
+        local variable = "fontSize"
+        local name = "Font Size"
+        local tooltip = "Control the font size"
+        local defaultValue = SessionTimeVars.fontSize
+        local setting = Settings.RegisterProxySetting(
+            category, "variable", SessionTimeVars, type(defaultValue), name, defaultValue
+        )
+        local options = Settings.CreateSliderOptions(1, 100, 1)
+        options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right);
+        Settings.CreateSlider(category, setting, options, tooltip)
+     end
+     -- offset x
+    do
+        local variable = "offsetX"
+        local name = "Offset X"
+        local tooltip = "Move the Text left or right"
+        local defaultValue = SessionTimeVars.offsetX
+        local setting = Settings.RegisterProxySetting(
+            category, variable, SessionTimeVars, type(defaultValue), name, defaultValue
+        )
+        local options = Settings.CreateSliderOptions(-100, 100, 1)
+        options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right);
+        Settings.CreateSlider(category, setting, options, tooltip)
+     end
+      -- offset y
+    do
+        local variable = "offsetY"
+        local name = "Offset Y"
+        local tooltip = "Move the Text up or down"
+        local defaultValue = SessionTimeVars.offsetY
+        local setting = Settings.RegisterProxySetting(
+            category, variable, SessionTimeVars, type(defaultValue), name, defaultValue
+        )
+        local options = Settings.CreateSliderOptions(-100, 100, 1)
+        options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right);
+        Settings.CreateSlider(category, setting, options, tooltip)
+     end
+    
+    Settings.RegisterAddOnCategory(category)
+end
+
+f:SetScript("OnEvent", function(self, event, addonName)
+    if (event == "ADDON_LOADED" and addonName == "SessionTime") then
+        createAddonSetting()
+    end
+end)
+
 f:SetScript("OnUpdate", function()
+    t:SetTextColor(255, 255, 255, 0.7)
+    t:SetPoint("BOTTOMLEFT", CharacterMicroButton, "LEFT", SessionTimeVars.offsetX, SessionTimeVars.offsetY)
+    t:SetFont("Fonts\\FRIZQT__.TTF", SessionTimeVars.fontSize, nil)
     t:SetText(SecondsToTime(GetSessionTime(), true))
 end)
